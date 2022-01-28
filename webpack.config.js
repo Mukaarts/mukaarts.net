6c5edbbc93f2395f37c9ece56210e1203abdf77e
mukaarts <fmichael.1995@gmail.com>
2022-01-29 00:20:10 +0100

    build: install tailwind #2

--- webpack.config.js
+++ webpack.config.js
@@ -56,8 +56,9 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
+    .enablePostCssLoader()
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
