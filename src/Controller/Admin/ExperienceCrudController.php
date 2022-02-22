be3e5d907a13130772fff59eb63a4bff17662e12
Michael Ferreira <fmichael.1995@gmail.com>
2022-02-22 19:06:41 +0100

    Admin dashboard (#25)
    * feat(admin): create dashboard
    
    Closes #18
    
    * feat(admin): create Experience
    
    Closes #19
    
    * feat(admin): create EmploymentType
    
    Closes #22
    
    * feat(admin): create Experience
    
    Closes #19

---
+++ src/Controller/Admin/ExperienceCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Experience;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class ExperienceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Experience::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('title'),
+            AssociationField::new('employmentType'),
+            TextField::new('company'),
+            TextField::new('location'),
+            Datefield::new('startDate'),
+            Datefield::new('endDate'),
+        ];
+    }
+}
