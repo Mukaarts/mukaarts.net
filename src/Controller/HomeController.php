66f756b8e03324b3fd7002696a2a6b38e1b06598
mukaarts <fmichael.1995@gmail.com>
2022-02-24 17:39:42 +0100

    refactor: home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\EmploymentType;
+use App\Repository\EmploymentTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(): Response
+    /**
+     * @param EmploymentTypeRepository<EmploymentType> $employmentTypeRepository
+     *
+     * @return Response
+     */
+    #[Route(path: '/', name: 'home')]
+    public function index(EmploymentTypeRepository $employmentTypeRepository): Response
     {
+        /** @var EmploymentType $employmentTypes */
+        $employmentTypes = $employmentTypeRepository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'employmentTypes' => $employmentTypes,
         ]);
     }
 }
