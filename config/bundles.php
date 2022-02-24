cf66f6bf8e1ee2f2f99a4c4d50f78aea1f529689
Michael Ferreira <fmichael.1995@gmail.com>
2022-02-24 21:06:42 +0100

    Implement basic Translations (#26)
    * build(composer): install knplabs/doctrine-behaviors & a2lix/translation-form-bundle
    
    * build: config a2lix
    
    * build(rout): add rout prefix
    
    * feat(admin): add TranslationField
    
    * feat(multilang): translate entities
    
    * build(multilang): generate translate fr files
    
    * refactor: home

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    A2lix\AutoFormBundle\A2lixAutoFormBundle::class => ['all' => true],
+    A2lix\TranslationFormBundle\A2lixTranslationFormBundle::class => ['all' => true],
+    Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
 ];
--- src/Repository/ExperienceRepository.php
+++ src/Repository/ExperienceRepository.php
@@ -7,10 +7,8 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Experience|null find($id, $lockMode = null, $lockVersion = null)
- * @method Experience|null findOneBy(array $criteria, array $orderBy = null)
- * @method Experience[]    findAll()
- * @method Experience[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @template T
+ * @extends ServiceEntityRepository<Experience>
  */
 class ExperienceRepository extends ServiceEntityRepository
 {
@@ -18,33 +16,4 @@ class ExperienceRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Experience::class);
     }
-
-    // /**
-    //  * @return Experience[] Returns an array of Experience objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('e.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Experience
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
