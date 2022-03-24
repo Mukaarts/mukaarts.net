7c875e2787f5a2a4d4c120fc3666f9ecfd0a6c67
mukaarts <fmichael.1995@gmail.com>
2022-03-24 17:55:19 +0100

    feat(education): create education entity
    Closes #20

---
+++ src/Repository/EducationRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Education;
+use App\Interface\Repository\EducationInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @template T
+ * @extends ServiceEntityRepository<Education>
+ */
+class EducationRepository extends ServiceEntityRepository implements EducationInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Education::class);
+    }
+
+    public function add(Education $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    public function remove(Education $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+}
