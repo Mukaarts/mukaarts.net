2294ff000ced5ac5165db093adbb12f672741afe
mukaarts <fmichael.1995@gmail.com>
2022-03-24 16:03:14 +0100

    feat: create career experience view
    Closes #38

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CareerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route(path: '/', name: 'home')]
-    public function index(): Response
+    public function index(CareerRepository $careerRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'career_experience' => $careerRepository->findAll(),
         ]);
     }
 }
