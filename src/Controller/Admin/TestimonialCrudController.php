1c7c10c24c05c8fc059719127a4023923ab04564
mukaarts <fmichael.1995@gmail.com>
2022-03-28 21:23:21 +0200

    feat(certification): Create testimonial crud
    Closes #55

---
+++ src/Controller/Admin/TestimonialCrudController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Testimonial;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class TestimonialCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Testimonial::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('name');
+        yield TextField::new('position');
+        yield TextEditorField::new('testimonial');
+        yield Field::new('imageFile')->setFormType(VichImageType::class);
+    }
+}
