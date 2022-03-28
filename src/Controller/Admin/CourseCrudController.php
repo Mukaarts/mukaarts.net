f99b25119094686838ec69f1d69e48841820a6f4
mukaarts <fmichael.1995@gmail.com>
2022-03-28 16:06:07 +0200

    feat(certification): Create courses & skills view
    Closes #50

---
+++ src/Controller/Admin/CourseCrudController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Course;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CourseCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Course::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('title');
+        yield AssociationField::new('skills');
+        yield AssociationField::new('mentor');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,9 @@ use App\Entity\Career;
 use App\Entity\Certification;
 use App\Entity\Education;
 use App\Interface\Repository\CertificationInterface;
+use App\Interface\Repository\CourseInterface;
 use App\Interface\Repository\EducationInterface;
+use App\Interface\Repository\SkillInterface;
 use App\Repository\CareerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,13 +27,17 @@ class HomeController extends AbstractController
     public function index(
         CareerRepository $careerRepository,
         EducationInterface $education,
-        CertificationInterface $certification
+        CertificationInterface $certification,
+        CourseInterface $course,
+        SkillInterface $skill
     ): Response {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'career_experience' => $careerRepository->findAll(),
             'certifications' => $certification->findAll(),
+            'courses' => $course->findAllCourses(),
             'education_experience' => $education->findAll(),
+            'skills' => $skill->findAllSkills(),
         ]);
     }
 }
