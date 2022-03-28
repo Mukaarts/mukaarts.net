71495cab20c6d65b4a493b3256f5b79123a015f6
mukaarts <fmichael.1995@gmail.com>
2022-03-28 21:24:15 +0200

    feat(certification): Create testimonial view
    Closes #56

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use App\Entity\Career;
 use App\Entity\Certification;
+use App\Entity\Course;
 use App\Entity\Education;
+use App\Entity\Skill;
+use App\Entity\Testimonial;
 use App\Interface\Repository\CertificationInterface;
 use App\Interface\Repository\CourseInterface;
 use App\Interface\Repository\EducationInterface;
 use App\Interface\Repository\SkillInterface;
+use App\Interface\Repository\TestimonialInterface;
 use App\Repository\CareerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,17 +23,21 @@ class HomeController extends AbstractController
     /**
      * @param CareerRepository<Career>              $careerRepository
      * @param CertificationInterface<Certification> $certification
+     * @param CourseInterface<Course>               $course
      * @param EducationInterface<Education>         $education
+     * @param SkillInterface<Skill>                 $skill
+     * @param TestimonialInterface<Testimonial>     $testimonial
      *
      * @return Response
      */
     #[Route(path: '/', name: 'home')]
     public function index(
         CareerRepository $careerRepository,
-        EducationInterface $education,
         CertificationInterface $certification,
         CourseInterface $course,
-        SkillInterface $skill
+        EducationInterface $education,
+        SkillInterface $skill,
+        TestimonialInterface $testimonial
     ): Response {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
@@ -38,6 +46,7 @@ class HomeController extends AbstractController
             'courses' => $course->findAllCourses(),
             'education_experience' => $education->findAll(),
             'skills' => $skill->findAllSkills(),
+            'testimonials' => $testimonial->findAllTestimonials(),
         ]);
     }
 }
